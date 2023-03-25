2c4e6c6a41f76675b32668ce1772d4bf8d9db01a
Slavi Karaslavov <106228555+Slaviiiii@users.noreply.github.com>
2023-03-25 10:39:38 +0200

    Delete src directory

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-
-export const Footer = () => {
-    return (
-        <footer>
-            <p>@Global Music</p>
-        </footer>
-    );
-}
--- src/services/authService.js
+++
@@ -1,5 +0,0 @@
-import * as request from "./requester";
-
-const baseurl = 'http://localhost:3030/users';
-
-export const login = async (loginData) => request.post(`${baseurl}/login`, loginData); 
