2249650122faf80aa392074a7c0525ee1d306623
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-03-26 22:12:15 +0300

    Created register functionality

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,15 +1,52 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForm";
 
 export const Register = () => {
+    const { onRegisterSubmit } = useContext(AuthContext);
+    const { values, changeHandler, onSubmit } = useForm({
+        email: '',
+        password: '',
+        ['re-password']: '',
+    }, onRegisterSubmit);
+
     return (
         <section id="register">
             <div className="form">
                 <h2>Register</h2>
-                <form method="POST" className="login-form">
-                    <input type="text" name="email" id="register-email" placeholder="email" />
-                    <input type="password" name="password" id="register-password" placeholder="password" />
-                    <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
+                <form method="POST" className="login-form" onSubmit={onSubmit}>
+                    <input
+                        type="text"
+                        name="email"
+                        id="register-email"
+                        placeholder="email"
+                        value={values.email}
+                    />
+
+                    <input
+                        type="password"
+                        name="password"
+                        id="register-password"
+                        placeholder="password"
+                        value={values.password}
+                        onChange={changeHandler}
+                    />
+
+                    <input
+                        type="password"
+                        name="re-password"
+                        id="repeat-password"
+                        placeholder="repeat password"
+                        value={values['re-password']}
+                        onChange={changeHandler}
+                    />
+
                     <button type="submit">Register</button>
-                    <p className="message">Already have an account? <a href="/login">Login</a></p>
+                    <p className="message">
+                        <span>Already have an account? <Link to={"/login"}>Log in now!</Link></span>
+                    </p>
                 </form>
             </div>
         </section>
