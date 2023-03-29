43957c9a36a79dd58a087a5cccc99124a2f5d42e
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-03-29 17:15:39 +0300

    Created musicServiceFactory

--- client/src/services/musicService.js
+++ client/src/services/musicService.js
@@ -1,32 +1,44 @@
-import * as request from "./requester";
+import { requestFactory } from "./requester";
 
 const baseUrl = 'http://localhost:3030/data/allMusic';
 
-export const getAll = async () => {
-    const result = await request.get(baseUrl);
-    const music = Object.values(result);
+export const musicServiceFactory = (token) => {
+    const request = requestFactory(token);
 
-    return music;
+    const getAll = async () => {
+        const result = await request.get(baseUrl);
+        const music = Object.values(result);
+
+        return music;
+    };
+
+    const create = async (musicData) => {
+        const result = await request.post(baseUrl, musicData);
+
+        return result;
+    };
+
+    const getOne = async (musicId) => {
+        const result = await request.get(`${baseUrl}/${musicId}`);
+
+        return result;
+    };
+
+    const edit = async (musicId, data) => {
+        const result = await request.put(`${baseUrl}/${musicId}`, data);
+
+        return result;
+    };
+
+    const deleteFunc = async (musicId) => {
+        await request.del(`${baseUrl}/${musicId}`);
+    }
+
+    return {
+        getAll,
+        create,
+        getOne,
+        edit,
+        deleteFunc
+    }
 };
-
-export const create = async (musicData, token) => {
-    const result = await request.post(baseUrl, musicData, token);
-
-    return result;
-};
-
-export const getOne = async (musicId) => {
-    const result = await request.get(`${baseUrl}/${musicId}`);
-
-    return result;
-};
-
-export const edit = async (musicId, data) => {
-    const result = await request.put(`${baseUrl}/${musicId}`, data);
-
-    return result;
-};
-
-export const deleteFunc = async (musicId) => {
-    await request.del(`${baseUrl}/${musicId}`);
-}
