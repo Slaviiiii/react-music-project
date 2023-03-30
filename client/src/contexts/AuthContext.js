c2aa470bc970c9ed04f866480fcae934598024f0
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-03-30 18:43:17 +0300

    Transferred authentication functionality to AuthContext

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,3 +1,61 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { authServiceFactory } from "./services/authService";
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = useState({});
+    const authService = authServiceFactory(auth.accessToken);
+
+    const onLoginSubmit = async (data) => {
+        try {
+            const result = await authService.login(data, auth.accessToken);
+
+            setAuth(result);
+            navigate('/');
+        } catch (err) {
+            console.log('Login problem!');
+        }
+    };
+
+    const onRegisterSubmit = async (data) => {
+        const { 're-password': repeatPassword, ...registerData } = data;
+        if (repeatPassword !== registerData.password) {
+            return;
+        }
+
+        try {
+            const result = await authService.register(registerData, auth.accessToken);
+
+            setAuth(result);
+            navigate('/');
+        } catch (err) {
+            console.log('Register problem!');
+        }
+    };
+
+    const onLogout = async () => {
+        await authService.logout(auth.accessToken);
+
+        setAuth({});
+    };
+
+    const context = {
+        onLoginSubmit,
+        onRegisterSubmit,
+        onLogout,
+        userId: auth._id,
+        token: auth.accessToken,
+        userEmail: auth.email,
+        isAuthenticated: !!auth.accessToken,
+    };
+
+    <AuthContext.Provider value={context}>
+        {children}
+    </AuthContext.Provider>
+}
