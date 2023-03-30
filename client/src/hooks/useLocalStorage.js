cc97ace91a3dd8fe1ab723c48b982b81bad0116b
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-03-30 19:52:43 +0300

    Updated useLocalStorage

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
-    const [state, setState] = useState(initialValue);
+    const [state, setState] = useState(() => {
+        const localStorageState = localStorage.getItem(key);
+        if (localStorageState) {
+            const parsedState = JSON.parse(localStorageState);
+
+            return parsedState;
+        }
+
+        return initialValue;
+    });
 
     const setLocalStorageState = (value) => {
         setState(value);
