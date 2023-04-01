f615aae52228d019338b35bdb78ee7838ac2ea60
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-01 21:35:38 +0300

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
-import { useState, useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-
-import { musicServiceFactory } from "./services/musicService";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-import { useLocalStorage } from "./hooks/useLocalStorage";
+import { MusicProvider } from "./contexts/MusicContext";
 
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
@@ -18,71 +15,35 @@ import { Logout } from "./components/Logout/Logout";
 import { RouteGuard } from "./components/common/RouteGuard";
 
 function App() {
-    const navigate = useNavigate();
-    const [music, setMusic] = useState([]);
-
-    const musicService = musicServiceFactory();
-
-    useEffect(() => {
-        musicService.getAll()
-            .then(result => {
-                setMusic(result);
-            })
-    }, []);
-
-    const onDelete = async (musicId) => {
-        const result = window.confirm("Are you sure you want to delete this music?");
-
-        if (result === true) {
-            await musicService.deleteFunc(musicId);
-
-            setMusic(state => state.filter(x => x._id !== musicId));
-            navigate('/allMusic');
-        }
-    };
-
-    const onMusicCreateSubmit = async (data) => {
-        const newMusic = await musicService.create(data);
-
-        navigate('/allMusic');
-        setMusic(state => [...state, newMusic]);
-    };
-
-    const onMusicEditSubmit = async (data) => {
-        const editedMusic = await musicService.edit(data._id, data);
-
-        navigate(`/details/${data._id}`);
-        setMusic(state => state.filter(x => x._id !== editedMusic._id));
-        setMusic(state => [...state, editedMusic]);
-    };
-
     return (
         <AuthProvider>
-            <div id="wrapper">
-                <Header />
-                <main>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/allMusic" element={<AllMusic music={music} />} />
-                        <Route path="/details/:musicId" element={<Details onDelete={onDelete} />} />
+            <MusicProvider>
+                <div id="wrapper">
+                    <Header />
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/allMusic" element={<AllMusic />} />
+                            <Route path="/details/:musicId" element={<Details />} />
 
-                        <Route element={<RouteGuard />}>
-                            <Route path="/logout" element={<Logout />} />
-                        </Route>
+                            <Route element={<RouteGuard />}>
+                                <Route path="/logout" element={<Logout />} />
+                            </Route>
 
-                        <Route element={<RouteGuard />}>
-                            <Route path="/create" element={<Create onMusicCreateSubmit={onMusicCreateSubmit} />} />
-                        </Route>
+                            <Route element={<RouteGuard />}>
+                                <Route path="/create" element={<Create />} />
+                            </Route>
 
-                        <Route element={<RouteGuard />}>
-                            <Route path="/edit/:musicId" element={<Edit onMusicEditSubmit={onMusicEditSubmit} />} />
-                        </Route>
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
+                            <Route element={<RouteGuard />}>
+                                <Route path="/edit/:musicId" element={<Edit />} />
+                            </Route>
+                        </Routes>
+                    </main>
+                    <Footer />
+                </div>
+            </MusicProvider>
         </AuthProvider>
     );
 }
