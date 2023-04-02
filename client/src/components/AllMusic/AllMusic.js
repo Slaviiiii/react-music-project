14d0ee5a07e3f329364602a58da853459a4f4a85
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-02 10:59:59 +0300

    Updated allMusic titles

--- client/src/components/AllMusic/AllMusic.js
+++ client/src/components/AllMusic/AllMusic.js
@@ -7,14 +7,14 @@ export const AllMusic = () => {
 
     return (
         <section id="all-music">
-            <h2>All Music</h2>
+            <h2>All Created Music</h2>
 
             {music.map(x =>
                 <Music key={`${x._id}-${x.artist}-${x.name}`} {...x} />
             )}
 
             {music.length === 0 && (
-                <h2>There is no music yet.</h2>
+                <h2>There are no creations on this page.</h2>
             )}
         </section>
     );
