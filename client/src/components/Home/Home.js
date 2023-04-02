d4d08ae98071bcb77c8abd4534d04d0b10352093
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-02 17:13:50 +0300

    Small changes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,4 @@
 
-// export const Home = () => {
-//     return (
-//         <section id="home">
-//             <h2>Are you looking for great music?</h2>
-//             <h3>This is the right place for you!</h3>
-//             <img src="/images/headphones.jpg" alt="headphones" />
-//         </section>
-//     );
-// };
-
 export const Home = () => {
     return (
         <div className="home-page">
