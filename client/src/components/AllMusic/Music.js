c2c3642be8071926724ea713ac4897f36e7a334e
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-02 16:48:04 +0300

    Removed comment

--- client/src/components/AllMusic/Music.js
+++ client/src/components/AllMusic/Music.js
@@ -21,21 +21,3 @@ export const Music = ({
         </div>
     );
 };
-
-// export const Music = ({
-//     name,
-//     imgUrl,
-//     artist,
-//     _id
-// }) => {
-//     return (
-//         <div className="music">
-//             <img src={imgUrl} alt={`${artist}--${name}`} />
-//             <p>
-//                 <strong>Name: </strong><span className="name">{name}</span>
-//             </p>
-//             <p><strong>Artist:</strong><span className="artist">{artist}</span></p>
-//             <Link to={`/details/${_id}`} className="details-btn">Details</Link>
-//         </div>
-//     );
-// };
