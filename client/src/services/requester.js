44821866cb28ab9944077a1dfd8579409167aa48
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-02 11:32:03 +0300

    Fixed accessToken in requester

--- client/src/services/requester.js
+++ client/src/services/requester.js
@@ -1,4 +1,4 @@
-const request = async (method, token, url, data) => {
+const request = async (method, url, data) => {
     const options = {};
 
     if (method !== 'GET') {
@@ -12,10 +12,12 @@ const request = async (method, token, url, data) => {
         }
     }
 
-    if (token) {
+    const serializedAuth = localStorage.getItem("auth");
+    if (serializedAuth) {
+        const auth = JSON.parse(serializedAuth);
         options.headers = {
             ...options.headers,
-            'X-Authorization': token,
+            'X-Authorization': auth.accessToken,
         };
     }
 
@@ -45,21 +47,12 @@ const request = async (method, token, url, data) => {
     }
 }
 
-export const requestFactory = (token) => {
-    if (!token) {
-        const serializedAuth = localStorage.getItem("auth");
-
-        if (serializedAuth && serializedAuth !== {}) {
-            const auth = JSON.parse(serializedAuth);
-            token = auth.accessToken;
-        }
-    }
-
+export const requestFactory = () => {
     return {
-        get: request.bind(null, 'GET', token),
-        post: request.bind(null, 'POST', token),
-        put: request.bind(null, 'PUT', token),
-        del: request.bind(null, 'DELETE', token),
-        patch: request.bind(null, 'PATCH', token)
+        get: request.bind(null, 'GET'),
+        post: request.bind(null, 'POST'),
+        put: request.bind(null, 'PUT'),
+        del: request.bind(null, 'DELETE'),
+        patch: request.bind(null, 'PATCH')
     }
 };
