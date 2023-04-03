17ad7dc2d25e5a4a9fff4d01d062aca293669095
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-03 21:49:22 +0300

    Updated useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -10,7 +10,7 @@ export const useForm = (initialValues, onSubmitHandler) => {
 
     const onSubmit = (e) => {
         e.preventDefault();
-
+        setValues(initialValues);
         onSubmitHandler(values);
     };
 
