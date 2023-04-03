b6d9c0fb0b121d9027b9baf88038ba8bb5f70eb8
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-03 21:49:44 +0300

    Deleted useService.js

--- client/src/hooks/useService.js
+++
@@ -1,8 +0,0 @@
-
-export const useService = (serviceFactory) => {
-    const auth = JSON.parse(localStorage.getItem("auth"));
-
-    const service = serviceFactory(auth.accessToken);
-
-    return service;
-};
