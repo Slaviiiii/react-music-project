a5bf033f046b03a6f7f016fad2aafcb0b93feef2
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-04 17:50:11 +0300

    Deleted Comments.js comments

--- client/src/components/Details/Comment/Comment.js
+++ client/src/components/Details/Comment/Comment.js
@@ -18,8 +18,6 @@ export const Comment = ({
                     <>
                         <input type="image" src="../images/pencil.png" id="pencil"></input>
                         <input type="image" src="../images/garbage-bin-icon-15.jpg" onClick={() => onCommentDelete(_id)} id="garbage"></input>
-                        {/* <img id="pencil" src="/images/pencil.png" alt="pencil" /> */}
-                        {/* <img onClick={() => onCommentDelete(_id)} id="garbage"  src="/images/garbage-bin-icon-15.jpg" alt="garbage" /> */}
                     </>
                 )}
             </p>
