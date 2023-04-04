1e4c99b23e0baaa1c53336b41a1aea57efea6a55
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-04 17:49:09 +0300

    Updated Comment.js

--- client/src/components/Details/Comment/Comment.js
+++ client/src/components/Details/Comment/Comment.js
@@ -16,8 +16,8 @@ export const Comment = ({
                 <input type="text" value={comment} size="60" disabled />
                 {isOwner && (
                     <>
-                        <button id="pencil"></button>
-                        <button onClick={() => onCommentDelete(_id)} id="garbage"></button>
+                        <input type="image" src="../images/pencil.png" id="pencil"></input>
+                        <input type="image" src="../images/garbage-bin-icon-15.jpg" onClick={() => onCommentDelete(_id)} id="garbage"></input>
                         {/* <img id="pencil" src="/images/pencil.png" alt="pencil" /> */}
                         {/* <img onClick={() => onCommentDelete(_id)} id="garbage"  src="/images/garbage-bin-icon-15.jpg" alt="garbage" /> */}
                     </>
