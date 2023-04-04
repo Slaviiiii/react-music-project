f87777512a7e3ead07db7a851fe82f3ffc70c12b
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-04 16:37:58 +0300

    Updated Details comments.css and js

--- client/src/components/Details/Comment/Comment.js
+++ client/src/components/Details/Comment/Comment.js
@@ -5,7 +5,12 @@ export const Comment = ({
 }) => {
     return (
         <li className="comment">
-            <p><span>{username}: {comment} </span><img src="/images/pencil.png" alt="pencil" /></p>
+            <p>
+                <span>{username}: </span>
+                <textarea name="comment-text" id="comment-text" value={comment} maxLength="103" cols="30" rows="1" disabled />
+                <img src="/images/pencil.png" alt="pencil" />
+                <img src="/images/garbage-bin-icon-15.jpg" alt="garbage" />
+            </p>
         </li>
     );
 };
