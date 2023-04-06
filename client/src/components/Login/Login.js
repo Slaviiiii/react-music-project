1ac1982190a3ec448557e781767759b63794ac11
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-06 21:11:58 +0300

    Updated login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,38 +1,72 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../../contexts/AuthContext";
-import { useForm } from "../../hooks/useForm";
+// import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { AuthContext } from "../../contexts/AuthContext";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useForm } from "react-hook-form";
+
+import { authServiceFactory } from "../../services/authService";
 
 export const Login = () => {
-    const { onLoginSubmit } = useContext(AuthContext);
-    const { values, changeHandler, onSubmit } = useForm({
-        email: '',
-        password: '',
-    }, onLoginSubmit);
+    // const { onLoginSubmit } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const [auth, setAuth] = useLocalStorage("auth", {});
+    const authService = authServiceFactory();
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors }
+    } = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+            loginError: "",
+        }
+    });
+
+    const onLoginSubmit = async (data) => {
+        const result = await authService.login(data);
+
+        if (result.accessToken) {
+            setAuth(result);
+            navigate('/');
+            return;
+        }
+
+        if (result === "Login or password don't match") {
+            setError('loginError', {
+                type: "custom",
+                message: "Login or password don't match!"
+            });
+        }
+    };
 
     return (
         <section id="login" >
             <div className="form">
                 <h2>Login</h2>
-                <form className="login-form" method="POST" onSubmit={onSubmit}>
-                    <input
-                        type="text"
-                        name="email"
-                        d="email"
+                <form className="login-form" method="POST" onSubmit={handleSubmit(onLoginSubmit)}>
+                    <p id="error-p">{errors.loginError?.message}</p>
+                    <input type="text"
+                        {...register("email", {
+                            required: "This field is required!",
+                            pattern: {
+                                // value: /^[a-zA-z]{3,}+@[[a-zA-z]+\.[a-zA-z]+$/,
+                                message: "Your email is incorrect!"
+                            }
+                        })}
+                        id="email"
                         placeholder="email"
-                        value={values.email}
-                        onChange={changeHandler}
                     />
-
-                    <input
-                        type="password"
-                        name="password"
+                    {errors.email?.message}
+                    <input type="password"
+                        {...register("password", {
+                            required: "This field is required!",
+                        })}
                         id="password"
                         placeholder="password"
-                        value={values.password}
-                        onChange={changeHandler}
                     />
-
+                    {errors.password?.message}
                     <button type="submit">Login</button>
                     <p className="message">
                         <span>Dont have an account? <Link to={"/register"}>Create one!</Link></span>
