32a9ec7d85e35c5cba8322716f07b71b560d5010
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-06 21:11:33 +0300

    Updated AddComment functionality

--- client/src/components/Details/AddComment/AddComment.js
+++ client/src/components/Details/AddComment/AddComment.js
@@ -11,8 +11,8 @@ export const AddComment = ({
         <div>
             <form className="form" onSubmit={onSubmit}>
                 <label>Add a comment:</label>
-                <textarea name="comment" placeholder="Comment......" rows="3" maxLength="110" value={values.comment} onChange={changeHandler} />
-                <input id="add-comment" type="submit" value="Add Comment" />
+                <textarea name="comment" rows="3" maxLength="110" value={values.comment} onChange={changeHandler} placeholder="Comment" />
+                <button id="add-comment" type="submit">Add Comment</button>
             </form>
         </div>
     );
