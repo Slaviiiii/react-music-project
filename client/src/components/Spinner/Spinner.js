af8f7349160b46839b25d41e4210cfa6d5733cc5
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-04-17 12:29:04 +0300

    Update

---
+++ client/src/components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import "./Spinner.css";
+
+export const Spinner = () => {
+    return (
+        <div class="spinner">
+            <div class="spinner-text">Global Music</div>
+        </div>
+    )
+}
